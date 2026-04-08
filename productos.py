f617df35049e5cc37c0c2f530eac9230bcc4767d
jelkin <jelkindavida@gmail.com>
2026-04-08 09:57:12 -0500

    Productos

---
+++ productos.py
@@ -0,0 +1,8 @@
+def agregar_producto(lista):
+    nombre = input("Nombre: ")
+    precio = float(input("Precio: "))
+    lista.append({"nombre": nombre, "precio": precio})
+
+def mostrar_productos(lista):
+    for p in lista:
+        print(p["nombre"], p["precio"])
